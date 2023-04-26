bb07e15c7337adaa5a5936e6d80f3a70431616b5
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-04-26 18:21:24 +0600

    Update Power.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int multipleVar = numberToPrint;
+        for (int count = 1; count < power; count++) {
+            numberToPrint *= multipleVar;
+        }
+        System.out.println(numberToPrint);
     }
 
     public static void main(String[] args) {
