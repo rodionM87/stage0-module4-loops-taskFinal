b6d5ab2cf0d1ff2d8541681db87d4147726193b0
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-04-26 17:45:11 +0600

    Update Pyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        int countStar = cathetusLength;
+        
+        for (int count = 1; count <= cathetusLength; count++) {
+            
+            for (int firstLimit = 1; firstLimit < countStar; firstLimit++) {
+                System.out.print(" ");
+            }
+            for (int secondLimit = countStar; secondLimit <= cathetusLength; secondLimit++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            countStar--;
+        }
     }
 
     public static void main(String[] args) {
