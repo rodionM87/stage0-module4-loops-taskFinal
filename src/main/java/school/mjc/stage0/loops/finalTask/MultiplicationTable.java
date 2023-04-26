454ec78ef4bb3f7db322f21985023d49334ba314
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-04-26 13:16:29 +0600

    Update MultiplicationTable.java

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        int count = 1;
+
+        for (int limit = 10; count <= limit; count++) {
+            int equals = count * numberTableToPrint;
+            System.out.println(count + " x " + numberTableToPrint + " = " + equals);
+        }
     }
 }
